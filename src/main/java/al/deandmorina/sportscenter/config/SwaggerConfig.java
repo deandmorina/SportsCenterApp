b5ab9ac3afdcb2bc022e7c9cf51589c0edc7f158
Deand Morina <morinadeand1@gmail.com>
2024-05-04 19:31:21 +0200

    Swagger Config Done

--- src/main/java/al/deandmorina/sportscenter/config/SwaggerConfig.java
+++ src/main/java/al/deandmorina/sportscenter/config/SwaggerConfig.java
@@ -1,11 +1,17 @@
 package al.deandmorina.sportscenter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,15 +22,23 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "APIs",
-                "Documentation",
-                "1.0.0",
-                "Terms Of Service",
-                new Contact("Deand Morina", "https://github.com/deandmorina", "morinadeand1@gmail.com"),
-                "Licence",
-                "URL",
-                Collections.emptyList()
-        );
+        return new ApiInfo("APIs", "Documentation", "1.0.0", "Terms Of Service", new Contact("Deand Morina", "https://github.com/deandmorina", "morinadeand1@gmail.com"), "Licence", "URL", Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).securityContexts(Collections.singletonList(securityContext())).securitySchemes(List.of(apiKey())).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
